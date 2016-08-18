31345893931bc8330eb43b186670f763f241bb3e
gabbydgab <gamba@gabbydgab.com>
2016-08-18 12:28:19 +0800

    initial test

---
+++ test/ConfigProviderTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * The MIT License
+ *
+ * Copyright (c) 2016, Coding Matters, Inc. (Gab Amba <gamba@gabbydgab.com>)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace CodingMatters\PersistenceTest;
+
+use PHPUnit\Framework\TestCase;
+use CodingMatters\Persistence\ConfigProvider;
+
+final class ConfigProviderTest extends TestCase
+{
+    /**
+     * @var CodingMatters\Persistence\ConfigProvider
+     */
+    private $provider;
+
+    public function setUp()
+    {
+        $this->provider = new ConfigProvider();
+    }    
+
+    /**
+     * @dataProvider getModuleConfigKeys
+     * @param array $key
+     */
+    public function testCpnfigProviderConfigKeyMustExist($key)
+    {        
+        $this->assertArrayHasKey($key, $this->provider->__invoke());
+        $this->assertTrue(is_array($this->provider->__invoke()[$key]));
+    }
+
+    public function getModuleConfigKeys()
+    {
+        return [
+            'dependencies' => ['dependencies']
+        ];
+    }
+
+    /**
+     * @dataProvider getServiceConfigKeys
+     * @param array $keys
+     */
+    public function testServiceManagerConfig($key)
+    {
+        $this->assertArrayHasKey($key, $this->provider->getServiceConfig());
+        $this->assertTrue(is_array($this->provider->getServiceConfig()[$key]));
+    }
+
+    public function getServiceConfigKeys()
+    {
+        return [
+            'invokables'    => ['invokables'],
+            'factories'     => ['factories'],
+            'delegators'    => ['delegators']
+        ];
+    }
+}
